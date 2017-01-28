6484be3cbc70f8f93812a68eb3f34690ed39c935
Abdelmageed Sayed <abdelmageedsayed@hotmail.com>
2017-01-28 15:49:55 +0200

    Graph nodes.

--- app/chart.js
+++ app/chart.js
@@ -1,4 +1,6 @@
 import * as d3 from 'd3'
+import './flags.css'
+//import './flags.png'
 
 const dataUrl = 'https://raw.githubusercontent.com/DealPete/forceDirected/master/countries.json'
 const chart = () => {
@@ -12,21 +14,31 @@ const chart = () => {
             .force('link', d3.forceLink(json.links).distance(50))
             .force('center', d3.forceCenter(480, 300))
             .force('charge', d3.forceManyBody()
-                    .strength(-50)
+                    .strength(-30)
                     .distanceMin(25)
                     .distanceMax(100))
+            .force('collision', d3.forceCollide(16)
+                    .strength(0.25))
         
         let link = svg.selectAll('.link')
             .data(json.links).enter()
             .append('line')
             .attr('class', 'link')
         
+        let node = d3.select('body').append('div')
+            .attr('class', 'node-container')
+            .data(json.nodes).enter()
+            .append('img')
+            .attr('class', d=>`node flag flag-${d.code}`)
+
         force.on('tick', ()=>{
             link.attr('x1', d=>d.source.x)
                 .attr('y1', d=>d.source.y)
                 .attr('x2', d=>d.target.x)
                 .attr('y2', d=>d.target.y)
-
+            
+            node.style('left', d=>d.x - 8 + 'px')
+                .style('top', d=>d.y - 6.5 + 'px')
         })
         
 //        force.start()
